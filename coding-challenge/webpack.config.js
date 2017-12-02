945190c8bcb0a6ec9716222b0ca57bfd8e23a044
Bubbanoosh <dev@bubbanoosh.com.au>
2017-12-02 17:26:31 +1100

    coding-challenge: Bablel & Webpack integration

---
+++ coding-challenge/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                query: {
+                  presets: ['react', 'es2015', 'stage-1']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000
+                }
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map'
+}
